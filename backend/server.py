a14b361a4f8d718c6ef514bf430bd7bb5fc3baa8
Taylor Dolezal <onlydole@gmail.com>
2023-12-28 12:17:46 -0800

    Enhance Server directives
    Signed-off-by: Taylor Dolezal <onlydole@gmail.com>

--- backend/server.py
+++ backend/server.py
@@ -19,18 +19,11 @@ app = FastAPI()
 
 app.mount("/site", StaticFiles(directory="./frontend"), name="site")
 app.mount("/static", StaticFiles(directory="./frontend/static"), name="static")
+app.mount("/outputs", StaticFiles(directory="outputs"), name="outputs")
 
 templates = Jinja2Templates(directory="./frontend")
 
 manager = WebSocketManager()
-
-
-# Dynamic directory for outputs once first research is run
-@app.on_event("startup")
-def startup_event():
-    if not os.path.isdir("outputs"):
-        os.makedirs("outputs")
-    app.mount("/outputs", StaticFiles(directory="outputs"), name="outputs")
 
 @app.get("/")
 async def read_root(request: Request):
@@ -39,7 +32,6 @@ async def read_root(request: Request):
 @app.get("/health")
 async def health_check():
     return {"status": "alive"}
-
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -66,4 +58,3 @@ async def websocket_endpoint(websocket: WebSocket):
 
     except WebSocketDisconnect:
         await manager.disconnect(websocket)
-
